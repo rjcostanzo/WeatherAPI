9472c70df718bc269f303695d83e105dc75562a0
rjcostanzo <rjcostanzo@gmail.com>
2021-09-01 20:27:51 -0500

    Uploading to repo

---
+++ assets/script/script.js
@@ -0,0 +1,49 @@
+var currentUnix = new Date();
+var locationInput = '';
+
+var locationLatitude = 0;
+var locationLongitude = 0;
+
+var 
+
+function locationPrompt() {
+    locationInput = prompt("Please enter a city, state, or country: ");
+    fetchInformation(locationInput);
+}
+
+
+function fetchInformation() {
+    
+    fetch(
+        'http://api.openweathermap.org/geo/1.0/direct?q=' + locationInput + '&limit=3&appid=8f0b1cc9479ed6c9966dcb4427c1b248'
+    )
+
+    .then(response => response.json())
+
+    .then(function(data) {
+        console.log(' ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ');
+        console.log('Coordinate Response: ');
+        console.log(data);
+        locationLatitude = data[0].lat;
+        locationLongitude = data[0].lon;
+        
+        console.log('Stored Coordinates: ' + locationLatitude + ' ' + locationLongitude);
+    
+        currentUnix = (Math.floor(currentUnix / 1000));
+
+        fetch(
+            'http://api.openweathermap.org/data/2.5/onecall/timemachine?lat=' + locationLatitude + '&lon=' + locationLongitude + '&dt=' + currentUnix + '&units=imperial&appid=8f0b1cc9479ed6c9966dcb4427c1b248'
+        )
+    
+        .then(response => response.json())
+        
+        .then(function(data) {
+            console.log(' ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ');
+            console.log('Weather Response: ');
+            console.log(data);
+            console.log(' ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ');
+        }); 
+
+    });
+
+}
